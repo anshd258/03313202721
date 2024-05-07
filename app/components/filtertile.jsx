723ef8745c369e33630c426bc70399d91cc81a4e
anshd258 <anshd258@gmail.com>
2024-05-07 12:30:26 +0530

    fixed minor bug

--- app/components/filtertile.jsx
+++ app/components/filtertile.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 
-function FilterTile({value}) {
+function FilterTile({ value }) {
     return (
-        <select value={"test"} className='px-3 py-1 md:px-5 rounded-lg border-2 border-gray-400 '>
+        <select value={"test"} className='px-3 py-1 mx-2 md:px-5 rounded-lg border-2 border-gray-400 '>
             {Object.entries(value).forEach(([key, value]) => (<option value={value[key]} selected >{key}</option>))}
 
 
